ee1cd19b6c88e71cd16cf92ff2846e2325297d8d
unkwnreaper <unkwnreaper@gmail.com>
2024-10-27 16:27:57 +0200

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,230 @@
+'use strict';
+
+const bcrypt      = require('bcrypt');
+const saltRounds = 12;
+const mongoose = require('mongoose');
+
+
+// mongoose setup
+mongoose.connect(process.env.MONGO_URI);
+const Schema = mongoose.Schema;
+// Schema setup
+const replySchema = new Schema({
+  text: {type: String, required: true},
+  delete_password: { type: String, required: true },
+  reported: { type: Boolean, default: false },
+  created_on: { type: Date, default: Date.now }
+});
+const threadSchema = new Schema({
+  text: {type: String, required: true},
+  delete_password: { type: String, required: true },
+  reported: { type: Boolean, default: false },
+  created_on: { type: Date, default: Date.now },
+  bumped_on: { type: Date, default: Date.now },
+  replies: { type: [replySchema]}
+});
+const boardSchema = new Schema({
+  name: { type: String, required: true },
+  threads: { type: [threadSchema] }
+});
+// Model setup
+const replyModel = mongoose.model('replyModel', replySchema);
+const threadModel = mongoose.model('threadModel', threadSchema);
+const boardModel = mongoose.model('boardModel', boardSchema);
+module.exports = boardSchema;
+module.exports = threadSchema;
+module.exports = replySchema;
+
+module.exports = function (app) {
+
+  app.route('/api/threads/:board')
+    .get(function(req, res) {
+      const board = req.params.board;
+      boardModel.findOne({name: board}).sort({ threads: -1 }).slice('threads', 10).then(brd => {
+          if (brd) {
+            if (brd.threads) {
+              res.json(brd.threads.map(thread => {
+                var {_id, text, created_on, bumped_on, replies } = thread;
+                if (replies.length > 3) {
+                  var replies = replies.slice(-3).map(rep => ({
+                    _id: rep._id,
+                    text: rep.text,
+                    created_on: rep.created_on
+                  }));
+                }
+                else {
+                  var replies = replies.map(rep => ({
+                    _id: rep._id,
+                    text: rep.text,
+                    created_on: rep.created_on
+                  }));}
+                var replyCount = replies.length;
+                return { _id, text, created_on, bumped_on, replies, replycount: replyCount };
+                })
+              );
+            }
+          }
+          else res.json({ error: "board not found" });
+      });
+    })
+    .post(async function(req, res) {
+      const { text, delete_password } = req.body;
+      const board = req.params.board;
+      const password = bcrypt.hashSync(delete_password, saltRounds);
+
+      const newThread = new threadModel({
+        text: text,
+        delete_password: password
+      });
+      await newThread.save();
+
+      boardModel.findOne({name: board}).then(async brd => {
+        if (brd) {
+          brd.threads.push(newThread);
+          await brd.save();
+        }
+        else {
+          const newBoard = new boardModel({
+            name: board,
+            threads: [newThread]
+          });
+          await newBoard.save();
+        }
+      });
+      res.redirect(`/b/${board}/${newThread._id}`);
+    })
+    .put(function(req, res) {
+      var thread_id = req.body.thread_id;
+      if (!thread_id) {
+        thread_id = req.query.thread_id;
+      }
+      const board = req.params.board;
+      boardModel.findOne({ name: board }).then(async brd => {
+        if (brd) {
+          var thread = brd.threads.id(thread_id);
+          if (thread) {
+            thread.reported = true;
+            thread.bumped_on = Date.now();
+            await brd.save();
+            res.send('reported');
+          }
+          else res.json({ error: "Thread not found" });
+        }
+        else res.json({ error: "Board not found" });
+      });
+    })
+    .delete(function(req, res) {
+      const { thread_id, delete_password } = req.body;
+      const board = req.params.board;
+      boardModel.findOne({ name: board }).then(async brd => {
+        if (brd) {
+          var thread = brd.threads.id(thread_id);
+          if (bcrypt.compareSync(delete_password, thread.delete_password)) {
+            thread.bumped_on = Date.now();
+            thread.replies.forEach(el => el.deleteOne());
+            thread.replies = [];
+            await brd.save();
+            res.send('success');
+          }
+          else {
+            res.send("incorrect password")
+          }
+        }
+        else res.json({ error: "Thread not found" });
+      });
+    });
+    
+  app.route('/api/replies/:board')
+  .get(function(req, res) {
+    const thread_id = req.query.thread_id;
+    const board = req.params.board;
+    boardModel.findOne({ name: board }).then(async brd => {
+      if (brd) {
+        const thread = brd.threads.id(thread_id);
+        if (thread) {
+          res.json({
+            _id: thread._id,
+            text: thread.text,
+            created_on: thread.created_on,
+            bumped_on: thread.bumped_on,
+            replies: thread.replies.map(reply => {
+              return {
+                _id: reply._id,
+                text: reply.text,
+                created_on: reply.created_on,
+              }})
+            });
+          }
+          else res.json({ error: "Thread not found" });
+        }
+      else res.json({ error: "Board not found" });
+      });
+    
+  })
+  .post(async function(req, res) {
+    const { thread_id, text, delete_password } = req.body;
+    const board = req.params.board;
+    const password = bcrypt.hashSync(delete_password, saltRounds);
+
+    const newReply = new replyModel({
+      text: text,
+      delete_password: password
+    });
+    await newReply.save();
+
+    boardModel.findOne({ name: board }).then(async brd => {
+      if (brd) {
+        var thread = brd.threads.id(thread_id);
+        if (thread) {
+          thread.bumped_on = newReply.created_on;
+          thread.replies.push(newReply);
+          await brd.save();
+          res.redirect(`/b/${board}/${thread_id}`);
+        }
+        else {
+          res.json({ error: "Thread not found" });
+        }
+      }
+      else {
+        res.json({ error: "Board not found" });
+      }
+      
+    });
+  })
+  .put(function(req, res) {
+    const { thread_id, reply_id } = req.body;
+    const board = req.params.board;
+    boardModel.findOne({name: board}).then(async brd => {
+      if (brd) {
+        var thread = brd.threads.id(thread_id);
+        var reply = thread.replies.id(reply_id);
+        reply.reported = true;
+        thread.bumped_on = Date.now();
+        await brd.save();
+        res.send("reported");
+      }
+      else {
+        res.json({ error: "Board not found" });
+      }
+    });
+  })
+  .delete(function(req, res) {
+    const { thread_id, reply_id, delete_password } = req.body;
+    const board = req.params.board;
+    boardModel.findOne({name: board}).then(async brd => {
+      if (brd) {
+        var thread = brd.threads.id(thread_id);
+        var reply = thread.replies.id(reply_id);
+        if (bcrypt.compareSync(delete_password, reply.delete_password)) {
+          thread.bumped_on = Date.now();
+          reply.text = '[deleted]';
+          await brd.save();
+          res.send("success");
+        } else {
+          res.send("incorrect password");
+        }
+      }
+    });
+  });
+
+};
---
+++ tests/2_functional-tests.js
@@ -0,0 +1,209 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+const mongoose = require('mongoose');
+const {boardSchema} = require('../routes/api');
+const boardModel = mongoose.model('boardModel', boardSchema);
+
+suite('Functional Tests', function() {
+    var brd
+    this.beforeEach(async function () {
+        brd = await boardModel.collection.findOne({name: 'general'});
+    });
+
+    test('Test POST /api/threads/{board}', function(done){
+        chai
+        .request(server)
+        .post('/api/threads/general/')
+        .send ({
+            "text": "Functional test 1",
+            "delete_password": "1234"
+        })
+        .end(function (err, res) {
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.isNotNull(brd, 'board not found');
+            assert.isArray(brd.threads, 'threads must be an array');
+            assert.isObject(brd.threads[0], 'must be object');
+            done();
+        });
+    });
+
+    test('Test GET /api/threads/{board}', function(done){
+        chai.request(server)
+        .get('/api/threads/general')
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.isArray(res.body, 'response must be an array');
+            assert.typeOf(res.body[0], 'object', 'response elements must be object');
+            assert.isBelow(res.body.length, 11, 'threads array must be 10 or less');
+            assert.isBelow(res.body[0].replies.length, 4, 'replies array must be 3 or less');
+            done();
+        });
+    });
+
+    test('Test DELETE /api/threads/{board} with the incorrect password', function(done){
+        chai.request(server)
+        .delete('/api/threads/general')
+        .send ({
+            "thread_id": brd.threads[0]._id, 
+            "delete_password": '3'
+        })
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.typeOf(res.text, 'string', 'response must be a string');
+            assert.equal(res.text, 'incorrect password', 'response must be correct text');
+            done();
+        });
+    });
+
+    test('Test DELETE /api/threads/{board} with the correct password', function(done){
+        const deleted = brd.threads[brd.threads.length - 1];
+        chai.request(server)
+        .delete('/api/threads/general')
+        .send ({
+            "thread_id": deleted._id, 
+            "delete_password": '1234'
+        })
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.typeOf(res.text, 'string', 'response must be a string');
+            assert.equal(res.text, 'success', 'response must be correct text');
+            assert.isEmpty(deleted.replies, 'database must be updated')
+            done();
+        });
+    });
+
+    test('Test PUT /api/threads/{board}', function(done){
+        const thread = brd.threads[brd.threads.length - 1];
+        chai.request(server)
+        .put(`/api/threads/general/?thread_id=${thread._id}`)
+        .send ({
+            "report_id": thread._id
+        })
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.typeOf(res.text, 'string', 'response must be a string');
+            assert.equal(res.text, 'reported', 'response must be correct text');
+            boardModel.collection.findOne({name: 'general'}).then(board => {
+                assert.isTrue(board.threads[board.threads.length - 1].reported, 'must be updated in database');
+                done();
+            })
+            
+        });
+    });
+
+    test('Test POST /api/replies/{board}', function(done){
+        const thread = brd.threads[brd.threads.length - 1];
+        chai
+        .request(server)
+        .post(`/api/replies/general/?thread_id=${thread._id}`)
+        .send ({
+            "thread_id": thread._id,
+            "text": "reply test 1",
+            "delete_password": "1234"
+        })
+        .end(function (err, res) {
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.isNotNull(thread, 'thread not found');
+            boardModel.collection.findOne({name: 'general'}).then(board => {
+                trd = board.threads[board.threads.length - 1];
+                assert.isArray(trd.replies, 'replies must be an array');
+                assert.typeOf(trd.replies[0], 'object', 'reply elements must be objects');
+                assert.equal(trd.replies[trd.replies.length - 1].text, "reply test 1", 'database must be updated correctly')
+                done();
+            })
+
+        });
+    });
+
+    test('Test GET /api/replies/{board}/{thread_id}', function(done){
+        const thread = brd.threads[brd.threads.length - 1];
+        chai
+        .request(server)
+        .get(`/api/replies/general/?thread_id=${thread._id}`)
+        .end(function (err, res) {
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.typeOf(res.body, 'object', 'response elements must be object');
+            assert.isArray(res.body.replies, 'replies must be an array');
+            assert.typeOf(res.body.replies[0], 'object', 'reply elements must be objects');
+            done();
+        });
+    });
+
+    test('Test DELETE /api/replies/{board} with the incorrect password', function(done){
+        const thread = brd.threads[brd.threads.length - 1];
+        const reply = thread.replies[thread.replies.length - 1];
+        chai.request(server)
+        .delete(`/api/replies/general/?thread_id=${thread._id}`)
+        .send ({
+            "thread_id": thread._id,
+            "reply_id": reply._id,
+            "delete_password": '3'
+        })
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.typeOf(res.text, 'string', 'response must be a string');
+            assert.equal(res.text, 'incorrect password', 'response must be correct text');
+            done();
+        });
+    });
+
+    test('Test DELETE /api/replies/{board} with the correct password', function(done){
+        const thread = brd.threads[brd.threads.length - 1];
+        const reply = thread.replies[thread.replies.length - 1];
+        chai.request(server)
+        .delete(`/api/replies/general/?thread_id=${thread._id}`)
+        .send ({
+            "thread_id": thread._id,
+            "reply_id": reply._id,
+            "delete_password": '1234'
+        })
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.typeOf(res.text, 'string', 'response must be a string');
+            assert.equal(res.text, 'success', 'response must be correct text');
+            boardModel.collection.findOne({name: 'general'}).then(board => {
+                trd = board.threads[board.threads.length - 1];
+                rep = trd.replies[trd.replies.length - 1];
+                assert.equal(rep.text, '[deleted]', 'database must be updated');
+                done();
+            });
+        });
+    });
+
+    test('Test PUT /api/replies/{board}', function(done){
+        const thread = brd.threads[brd.threads.length - 1];
+        const reported = thread.replies[thread.replies.length - 1];
+        chai.request(server)
+        .put(`/api/replies/general/?thread_id=${thread._id}`)
+        .send ({
+            "thread_id": thread._id,
+            "reply_id": reported._id
+        })
+        .end(function(err, res){
+            assert.equal(err, null, 'should be no errors');
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'reported', 'response must be correct text');
+            boardModel.collection.findOne({name: 'general'}).then(board => {
+                trd = board.threads[board.threads.length - 1];
+                rep = trd.replies[trd.replies.length - 1];
+                assert.typeOf(rep.text, 'string', 'response must be a string');
+                assert.isTrue(rep.reported, 'must be updated in database');
+                done();
+            });
+        });
+    });
+});
